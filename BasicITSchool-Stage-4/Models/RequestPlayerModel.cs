674add2f021afc352f6c4aedd98588e66d720906
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-05-22 13:34:24 +0400

    Models

---
+++ BasicITSchool-Stage-4/Models/RequestPlayerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicITSchool_Stage_4.Models
+{
+    public class RequestPlayerModel
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "None";
+        //public int MyProperty { get; set; }
+    }
+}
---
+++ BasicITSchool-Stage-4/Models/ResponsePlayerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicITSchool_Stage_4.Models
+{
+    public class ResponsePlayerModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
